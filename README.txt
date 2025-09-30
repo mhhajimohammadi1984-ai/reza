90ecfecfe73b1532ce72200d47b858f92234d269
Mohammad Hossein Haji Mohammadi <m.h.hajimohammadi1984@gmail.com>
2025-09-30 18:30:18 +0330

    test for git first change

---
+++ README.txt
@@ -0,0 +1,10 @@
+📌 اجرای پروژه HelloFX در VS Code
+
+1. پوشه JavaFX SDK خود را داخل فولدر lib کپی کنید 
+   (یا فقط مسیر درستش رو در launch.json بنویسید).
+
+2. در VS Code، کل پروژه HelloFX را باز کنید.
+
+3. از منوی Run → Start Debugging (یا F5) برنامه را اجرا کنید.
+
+✅ باید یک پنجره JavaFX با متن "سلام به JavaFX در VS Code 👋" باز شود.
---
+++ src/projectphase1v1/App.java
@@ -0,0 +1,22 @@
+package projectphase1v1;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("سلام به JavaFX در VS Code 👋");
+        Scene scene = new Scene(label, 400, 200);
+        stage.setScene(scene);
+        stage.setTitle("HelloFX");
+        stage.show();
+        //i have started using git
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
